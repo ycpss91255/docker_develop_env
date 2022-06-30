11a71b9137ea45d20b10c00eec8a4245028178c1
cyc <ycpss91255@gmail.com>
2022-06-30 11:38:20 +0800

    init : ubuntu env

---
+++ ubuntu20.04/ros&torch1.8.1&cuda11.1.1+gpu/docker/run-docker-container.bash
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+source $(pwd)/get_graph_card.bash
+# start sharing xhost
+xhost +local:root
+
+GRAPHCARD_DEBUG=false
+
+DOCKER_IMAGE_NAME=pytorch-ros
+DOCKER_CONTAINER_NAME=pytorch-ros
+
+WORKSPACE_PATH=$HOME/workspace/pytorch_ws
+
+IntelDockerRun(){
+  docker run --rm \
+    --net=host \
+    --ipc=host \
+    --privileged \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v $HOME/.Xauthority:$docker/.Xauthority \
+    -v ${WORKSPACE_PATH}:$HOME/work \
+    -v /dev:/dev \
+    -v /etc/timezone:/etc/timezone:ro \
+    -v /etc/localtime:/etc/localtime:ro \
+    -e XAUTHORITY=$home_folder/.Xauthority \
+    -e DISPLAY=$DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -it --name $DOCKER_CONTAINER_NAME $(id -un)/${DOCKER_IMAGE_NAME}
+}
+NvidiaDockerRun(){
+  docker run --rm \
+    --net=host \
+    --ipc=host \
+    --gpus all \
+    --privileged \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v $HOME/.Xauthority:$docker/.Xauthority \
+    -v ${WORKSPACE_PATH}:$HOME/work \
+    -v /dev:/dev \
+    -v /etc/timezone:/etc/timezone:ro \
+    -v /etc/localtime:/etc/localtime:ro \
+    -e XAUTHORITY=$home_folder/.Xauthority \
+    -e DISPLAY=$DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -it --name $DOCKER_CONTAINER_NAME $(id -un)/${DOCKER_IMAGE_NAME}
+}
+
+GetGraphCard $GRAPHCARD_DEBUG
+
+if [ $GraphicsCard == "INTEL" ] ; then
+  IntelDockerRun
+elif [ $GraphicsCard == "NVIDIA" ] ; then
+  NvidiaDockerRun
+else
+  echo "Error : Unknown Graphics Card!!"
+fi
