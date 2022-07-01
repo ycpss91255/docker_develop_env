a4c5e609994d694621957974ac1246e2d1476d98
cyc <ycpss91255@gmail.com>
2022-07-01 22:13:00 +0800

    adjust : all docker env and script

--- ubuntu18.04/ros/docker/config/script/pip_setup.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python -m pip install --upgrade --force-reinstall pip
-pip install -r ./pip/requirements.txt
--- ubuntu20.04/ros&torch1.8.1&cuda11.1.1/docker/config/script/zsh_setup.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mv ./zsh/powerlevel10k /home/${USER}/
-mv ./zsh/.p10k.zsh /home/${USER}/
-cat ./zsh/zshrc/* >> /home/${USER}/.zshrc
